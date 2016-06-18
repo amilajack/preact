74edb43d44a705edb03147cda74493f74600e92c
Jason Miller <jason@developit.ca>
2016-06-18 13:10:32 -0400

    Add built-in support for inline SVG (adds ~115b)

--- src/vdom/diff.js
+++ src/vdom/diff.js
@@ -9,6 +9,8 @@ import { createNode, collectNode } from '../dom/recycler';
 import { unmountComponent } from './component';
 
 
+let SVG_MODE = false;
+
 
 /** Apply differences in a given vnode (and it's deep children) to a real DOM Node.
  *	@param {Element} [dom=null]		A DOM node to mutate into the shape of the `vnode`
@@ -36,19 +38,26 @@ export function diff(dom, vnode, context, mountAll, unmountChildrenOnly) {
 		return document.createTextNode(vnode);
 	}
 
-	if (isFunction(vnode.nodeName)) {
+	let out = dom,
+		nodeName = vnode.nodeName,
+		svgMode;
+
+	if (isFunction(nodeName)) {
 		return buildComponentFromVNode(dom, vnode, context, mountAll);
 	}
+	if (!isString(nodeName)) {
+		nodeName = String(nodeName);
+	}
 
-	let out = dom,
-		nodeName = vnode.nodeName;
-	if (!isString(nodeName)) nodeName = String(nodeName);
+	svgMode = toLowerCase(nodeName)==='svg';
+
+	if (svgMode) SVG_MODE = true;
 
 	if (!dom) {
-		out = createNode(nodeName);
+		out = createNode(nodeName, SVG_MODE);
 	}
 	else if (!isNamedNode(dom, nodeName)) {
-		out = createNode(nodeName);
+		out = createNode(nodeName, SVG_MODE);
 		// move children into the replacement node
 		while (dom.firstChild) out.appendChild(dom.firstChild);
 		// reclaim element nodes
@@ -62,6 +71,8 @@ export function diff(dom, vnode, context, mountAll, unmountChildrenOnly) {
 	if (originalAttributes && originalAttributes.ref) {
 		(out[ATTR_KEY].ref = originalAttributes.ref)(out);
 	}
+
+	if (svgMode) SVG_MODE = false;
 
 	return out;
 }
@@ -232,7 +243,7 @@ function diffAttributes(dom, attrs) {
 	// removeAttributes(dom, old, attrs || EMPTY);
 	for (let name in old) {
 		if (!attrs || !(name in attrs)) {
-			setAccessor(dom, name, null);
+			setAccessor(dom, name, null, SVG_MODE);
 		}
 	}
 
@@ -242,7 +253,7 @@ function diffAttributes(dom, attrs) {
 			let value = attrs[name];
 			if (value===undefined) value = null;
 			if (!(name in old) || value!=old[name]) {
-				setAccessor(dom, name, value);
+				setAccessor(dom, name, value, SVG_MODE);
 			}
 		}
 	}
